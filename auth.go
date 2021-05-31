07050237c1ab8386bbed187669a8674d762d7360
Aryan Misra <aryanmisra4@gmail.com>
2021-05-31 11:17:10 -0400

    Update auth.go

--- auth.go
+++ auth.go
@@ -41,7 +41,7 @@ func internalServerAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		switch ENV_MODE {
 		case "release":
-			signature, ok := c.Request.Header["Server-Signature"]
+			signature, ok := c.Request.Header["x-server-signature"]
 			if !ok {
 				c.String(http.StatusBadRequest, "Where da signature at doe?")
 			}
