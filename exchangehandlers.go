7281dbb57b0588122e7aee073b4b65e3d4a6ff4b
Aryan Misra <aryanmisra4@gmail.com>
2021-06-13 22:33:45 -0700

    init

--- exchangehandlers.go
+++ exchangehandlers.go
@@ -17,6 +17,10 @@ import (
 	"v1.1-fulfiller/s3"
 )
 
+func OrderIDGen(orderType string, orderSide string, username string, quantity float64, created time.Time) (orderID string) {
+	return fmt.Sprintf("%s-%s-%s-%v-%v", orderType, orderSide, username, quantity, created.UnixNano()/int64(time.Millisecond))
+}
+
 func SanitizeHandler(c *gin.Context) {
 	var reqBody models.UsernameRequest
 	if err := c.ShouldBindWith(&reqBody, binding.JSON); err != nil {
---
+++ fireeye/fireeye.go
@@ -0,0 +1,5 @@
+package fireeye
+
+func SyncStatus() {
+
+}
