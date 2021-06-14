b15e8a8a47711b0833f20beadbb5665661ac9f41
Dhrumil Patel <dhrumilp15@gmail.com>
2021-06-14 10:28:55 -0400

    Only find our single BitClout Wallet Account

--- fireeye/fireeye.go
+++ fireeye/fireeye.go
@@ -3,6 +3,7 @@ package fireeye
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"math"
@@ -14,10 +15,9 @@ import (
 )
 
 type FireEyeT struct {
-	Message     string
-	Code int
+	Message string
+	Code    int
 }
-
 
 //0-10 CODE -> OK, Possible info
 //10-20 CODE -> Warn, Requests allowed still
@@ -27,9 +27,10 @@ var FireEye = &FireEyeT{
 	Message: "Pending Initialization",
 	Code:    20,
 }
+
 const OKTolerance = 0.0001 // 0.001% Tolerance
 const MidTolerance = 0.001 // 0.1% Tolerance
-const MaxTolerance = 0.01 //1% Tolerance
+const MaxTolerance = 0.01  //1% Tolerance
 
 const initBcltTolerance = -156.90016130600003
 const initEthTolerance = 24.346454580232383
@@ -48,7 +49,7 @@ func SyncStatus(ctx context.Context) {
 		log.Panic(err)
 		return
 	}
-	getUsersSLReqMap := map[string][]string{"PublicKeysBase58Check":config.Wallet.Addr_BCLT}
+	getUsersSLReqMap := map[string][]string{"PublicKeysBase58Check": config.Wallet.Addr_BCLT}
 	getUsersSLReqBody, err := json.Marshal(getUsersSLReqMap)
 	if err != nil {
 		SetSyncWarn(err)
@@ -56,15 +57,20 @@ func SyncStatus(ctx context.Context) {
 		return
 	}
 	getUserSLResp := new(models.GetUsersStateLessResponse)
-	if err := global.PostJson("https://bitclout.com/api/v0/get-users-stateless",getUsersSLReqBody,getUserSLResp); err != nil {
+	if err := global.PostJson("https://bitclout.com/api/v0/get-users-stateless", getUsersSLReqBody, getUserSLResp); err != nil {
 		SetSyncWarn(err)
 		log.Panic("ERROR getusersstateless: ", err)
 		return
 	}
-	var walletBcltBalance float64 = 0
-	for _,balance := range getUserSLResp.Userlist {
-		walletBcltBalance += float64(balance.BalanceNanos)/1e9
+	// We should only retrieve our single BitClout wallet account
+	if len(getUserSLResp.Userlist) != 1 {
+		SetSyncWarn(errors.New("could not find the Wallet BitClout Account"))
+		log.Panic("ERROR getusersstateless UserList too small")
 	}
+	var walletBcltBalance float64 = float64(getUserSLResp.Userlist[0].BalanceNanos) / 1e9
+	// for _, balance := range getUserSLResp.Userlist {
+	// 	walletBcltBalance += float64(balance.BalanceNanos) / 1e9
+	// }
 	pools, err := db.GetAllPools(ctx)
 	if err != nil {
 		SetSyncWarn(err)
@@ -72,18 +78,18 @@ func SyncStatus(ctx context.Context) {
 		return
 	}
 	var walletEthBalance float64 = 0
-	for _,pool := range pools {
+	for _, pool := range pools {
 		walletEthBalance += pool.Balance
 	}
 	var (
 		bitcloutSync = totalBalance.Bitclout + totalFees.Bitclout + initBcltTolerance
-		etherSync = totalBalance.Ether + totalFees.Ether + initEthTolerance
+		etherSync    = totalBalance.Ether + totalFees.Ether + initEthTolerance
 	)
 
-	if (math.Abs((bitcloutSync/walletBcltBalance)-1)>MaxTolerance) {
+	if math.Abs((bitcloutSync/walletBcltBalance)-1) > MaxTolerance {
 		FireEye.Code = 30
 		FireEye.Message = fmt.Sprintf("Bitclout balance out of sync.")
-	}else if (math.Abs((etherSync/walletEthBalance)-1)>MaxTolerance) {
+	} else if math.Abs((etherSync/walletEthBalance)-1) > MaxTolerance {
 		FireEye.Code = 31
 		FireEye.Message = fmt.Sprintf("Ether balance out of sync.")
 	} else {
