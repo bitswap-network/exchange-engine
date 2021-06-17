b0d5f01202b74a08990bc761cadcf25e59cf41dd
Aryan Misra <aryanmisra4@gmail.com>
2021-06-16 20:59:09 -0700

    Update fireeye.go

--- fireeye/fireeye.go
+++ fireeye/fireeye.go
@@ -31,7 +31,7 @@ const MidTolerance = 0.001 // 0.1% Tolerance
 const MaxTolerance = 0.01  //1% Tolerance
 
 const initBcltTolerance = -158.750162
-const initEthTolerance = 11.9689164
+const initEthTolerance = 11.8801843
 
 func SyncStatus(ctx context.Context) {
 
