d1904cd6835f17bc6b994e1801a341d0dbb9683f
Aryan Misra <aryanmisra4@gmail.com>
2021-06-27 23:23:38 -0400

    Update fireeye.go

--- fireeye/fireeye.go
+++ fireeye/fireeye.go
@@ -52,7 +52,7 @@ func SyncStatus(ctx context.Context) {
 		return
 	}
 	getUserSLResp := new(models.GetUsersStateLessResponse)
-	if err := global.PostJson("https://bitclout.com/api/v0/get-users-stateless", getUsersSLReqBody, getUserSLResp); err != nil {
+	if err := global.PostJson("http://node.bitswap.network/api/v0/get-users-stateless", getUsersSLReqBody, getUserSLResp); err != nil {
 		SetSyncWarn(err)
 		log.Panic("ERROR getusersstateless: ", err)
 		return
